61206e41cd81c845a47bc4c117f1285e514d47df
Tadas Knieza <tadasknieza@gmail.com>
2022-03-31 19:00:56 +0100

    add config files and tailwind support

---
+++ prettier.config.js
@@ -0,0 +1,10 @@
+// eslint-disable-next-line no-undef
+module.exports = {
+  semi: false,
+  bracketSpacing: true,
+  singleQuote: true,
+  tabWidth: 2,
+  trailingComma: 'none',
+  arrowParens: 'avoid',
+  endOfLine: 'lf'
+}
